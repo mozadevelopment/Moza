f9dd7cec2f614dd3f6a0308d781ba4cb7b2a382c
Ihara <mozadevelopment@gmail.com>
2021-04-04 10:20:18 -0300

    Fixed register activity.

--- app/src/main/java/com/mozadevelopment/moza/RegisterActivity.java
+++ app/src/main/java/com/mozadevelopment/moza/RegisterActivity.java
@@ -11,13 +11,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.hbb20.CountryCodePicker;
 
 import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
 
-    EditText editTextEmail, editTextPassword, editTextName;
-
+    EditText editTextEmail, editTextPassword, editTextName,editTextPhone;
+    CountryCodePicker ccp;
     private FirebaseAuth mAuth;
 
     @Override
@@ -28,6 +29,10 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         editTextName = findViewById(R.id.edit_text_name);
         editTextEmail = findViewById(R.id.edit_text_email);
         editTextPassword = findViewById(R.id.edit_text_password);
+        ccp = findViewById(R.id.ccp);
+        editTextPhone = findViewById(R.id.edit_text_phone);
+
+        ccp.registerCarrierNumberEditText(editTextPhone); //Unir codigo de pais con numero de telefono
 
         findViewById(R.id.button_register).setOnClickListener(this);
         findViewById(R.id.text_view_login).setOnClickListener(this);
@@ -37,14 +42,35 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
     private void registerUser(){
         String email = editTextEmail.getText().toString().trim();
+        String phone = editTextPhone.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
         String name = editTextName.getText().toString().trim();
-        String emailNeededToast = getString(R.string.emailNeededToast);
+        String phoneNeededToast = getString(R.string.phoneNeededToast);
         String emailValidToast = getString(R.string.emailValidToast);
         String passwordValidToast = getString(R.string.passwordValidToast);
         String passwordNeededToast = getString(R.string.passwordNeededToast);
         String nameNeededToast = getString(R.string.nameNeededToast);
         String alreadyRegisteredErrorToast = getString(R.string.alreadyRegisteredErrorToast);
+        String emailNeededToast = getString(R.string.emailNeededToast);
+        String phoneInvalidToast = getString(R.string.phoneInvalidToast);
+
+        if (name.isEmpty()){
+            editTextName.setError(nameNeededToast);
+            editTextName.requestFocus();
+            return;
+        }
+
+        if (phone.isEmpty()){
+            editTextPhone.setError(phoneNeededToast);
+            editTextPhone.requestFocus();
+            return;
+        }
+
+        if (!ccp.isValidFullNumber()){
+            editTextPhone.setError(phoneInvalidToast);
+            editTextPhone.requestFocus();
+            return;
+        }
 
         if (email.isEmpty()){
             editTextEmail.setError(emailNeededToast);
@@ -53,14 +79,8 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         }
 
         if (password.isEmpty()){
-            editTextEmail.setError(passwordNeededToast);
-            editTextEmail.requestFocus();
-            return;
-        }
-
-        if (name.isEmpty()){
-            editTextName.setError(nameNeededToast);
-            editTextName.requestFocus();
+            editTextPassword.setError(passwordNeededToast);
+            editTextPassword.requestFocus();
             return;
         }
 
@@ -81,7 +101,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 finish();
                 startActivity(new Intent(RegisterActivity.this, MainActivity.class));
             } else {
-
                 if (task.getException() instanceof FirebaseAuthUserCollisionException) {
                     Toast.makeText(getApplicationContext(), alreadyRegisteredErrorToast, Toast.LENGTH_SHORT).show();
 
