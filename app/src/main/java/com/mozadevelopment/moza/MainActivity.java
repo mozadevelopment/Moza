d26c40e4b09724198ac93cd28487f55407650be0
DieGovia <diego33segovia@hotmail.com>
2021-04-06 21:31:44 -0300

    Dependencias para scanner botones en Main

--- app/src/main/java/com/mozadevelopment/moza/MainActivity.java
+++ app/src/main/java/com/mozadevelopment/moza/MainActivity.java
@@ -2,13 +2,28 @@ package com.mozadevelopment.moza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button qr, makeAnOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        qr = (Button)findViewById(R.id.button_qr);
+        makeAnOrder = (Button)findViewById(R.id.button_make_an_order);
+
+        qr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
