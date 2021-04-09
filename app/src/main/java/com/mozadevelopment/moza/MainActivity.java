0100e2eca090ef7a3a8f8536a6ca7ac79e8575a9
DieGovia <diego33segovia@hotmail.com>
2021-04-08 23:00:17 -0300

    QR Scanner dependencies

--- app/src/main/java/com/mozadevelopment/moza/MainActivity.java
+++ app/src/main/java/com/mozadevelopment/moza/MainActivity.java
@@ -1,11 +1,11 @@
 package com.mozadevelopment.moza;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +22,12 @@ public class MainActivity extends AppCompatActivity {
         qr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+
             }
+
+
         });
     }
 }
