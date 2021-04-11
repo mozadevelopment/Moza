e37306665c690e6cd44d79bffef39e1cc596b437
DieGovia <diego33segovia@hotmail.com>
2021-04-11 16:36:34 -0300

    QR scanner is added New Java class for Scanner Orientation

---
+++ app/src/main/java/com/mozadevelopment/moza/CaptureActivityPortrait.java
@@ -0,0 +1,6 @@
+package com.mozadevelopment.moza;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureActivityPortrait extends CaptureActivity {
+}
--- app/src/main/java/com/mozadevelopment/moza/MainActivity.java
+++ app/src/main/java/com/mozadevelopment/moza/MainActivity.java
@@ -1,17 +1,25 @@
 package com.mozadevelopment.moza;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button qr, makeAnOrder;
+    private Button qr, makeAnOrder;
+    private TextView textViewQr, textViewQrFail;
+    String integratorPrompt = "Scan QR code";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +28,43 @@ public class MainActivity extends AppCompatActivity {
 
         qr = (Button)findViewById(R.id.button_qr);
         makeAnOrder = (Button)findViewById(R.id.button_make_an_order);
+        textViewQr = findViewById(R.id.text_view_qr);
+        textViewQrFail = findViewById(R.id.text_view_qr);
 
-        qr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                new IntentIntegrator(MainActivity.this).initiateScan();
-
-            }
-
-
-        });
+        qr.setOnClickListener(mOnClickListener);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null)
+            if (result.getContents() != null){
+                textViewQr.setText(result.getContents());
+            }else if (result.getContents() == null){
+            textViewQr.setText(R.string.textViewQr);
+            }else{
+                textViewQrFail.setText(R.string.textViewQrFail);
+            }
+    }
+
+    public void scan(){
+        IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+        integrator.setOrientationLocked(false);
+        integrator.setCaptureActivity(CaptureActivityPortrait.class);
+        integrator.setPrompt(getString(R.string.integratorPrompt));
+        integrator.initiateScan();
+    }
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.button_qr:
+                    scan();
+                    break;
+            }
+        }
+    };
+
 }
