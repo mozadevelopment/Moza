b543c7e1c53220283f001c1ce68c22a1f5e0f521
DieGovia <diego33segovia@hotmail.com>
2021-04-11 22:42:44 -0300

    added menu

--- app/src/main/java/com/mozadevelopment/moza/MainActivity.java
+++ app/src/main/java/com/mozadevelopment/moza/MainActivity.java
@@ -3,10 +3,13 @@ package com.mozadevelopment.moza;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,6 +35,30 @@ public class MainActivity extends AppCompatActivity {
         textViewQrFail = findViewById(R.id.text_view_qr);
 
         qr.setOnClickListener(mOnClickListener);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menu_logout:
+                logout();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logout(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
     }
 
     @Override
